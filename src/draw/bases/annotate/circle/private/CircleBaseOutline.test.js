1aab6acc6b55ffbbad6ea9fa1a80eda8aa0a89a0
Philip Johnson <p.zhao.johnson@gmail.com>
2023-02-05 10:50:14 -0500

    Initialize circle element IDs in CircleBaseOutline class constructor

--- src/draw/bases/annotate/circle/private/CircleBaseOutline.test.js
+++ src/draw/bases/annotate/circle/private/CircleBaseOutline.test.js
@@ -1,30 +1,62 @@
 import * as SVG from '@svgdotjs/svg.js';
 
+import { uuidRegex } from 'Utilities/uuidRegex';
+
 import { CircleBaseOutline } from './CircleBaseOutline';
 
 describe('CircleBaseOutline class', () => {
   describe('constructor', () => {
-    test('when no arguments are provided', () => {
-      let outline = new CircleBaseOutline();
-
-      expect(outline.circle).toBeInstanceOf(SVG.Circle);
-      expect(outline.cachedBaseCenter).toBeUndefined();
-    });
-
-    test('when only the circle element is provided', () => {
+    it('stores the provided circle element', () => {
       let circle = new SVG.Circle();
       let outline = new CircleBaseOutline({ circle });
-
       expect(outline.circle).toBe(circle);
+    });
+
+    it('creates a new circle element when one is not provided', () => {
+      let outline = new CircleBaseOutline();
+      expect(outline.circle).toBeInstanceOf(SVG.Circle);
+    });
+
+    it('caches the provided base center', () => {
+      let baseCenter = { x: 56.187, y: 903.752 };
+      let outline = new CircleBaseOutline({ baseCenter });
+      expect(outline.cachedBaseCenter).toStrictEqual(baseCenter);
+    });
+
+    test('when the base center is not provided', () => {
+      let outline = new CircleBaseOutline();
       expect(outline.cachedBaseCenter).toBeUndefined();
     });
 
-    test('when only the base center is provided', () => {
-      let baseCenter = { x: 1102.56, y: 948.74 };
-      let outline = new CircleBaseOutline({ baseCenter });
+    it('will initialize the ID of the provided circle element', () => {
+      let circle = new SVG.Circle();
+      expect(circle.attr('id')).toBeFalsy();
 
-      expect(outline.circle).toBeInstanceOf(SVG.Circle);
-      expect(outline.cachedBaseCenter).toEqual({ x: 1102.56, y: 948.74 });
+      let outline = new CircleBaseOutline({ circle });
+
+      expect(outline.circle.attr('id')).toMatch(uuidRegex);
+      expect(outline.circle.attr('id').length).toBeGreaterThanOrEqual(36);
+
+      // must start with a letter in SVG
+      expect(outline.circle.attr('id').startsWith('uuid-')).toBeTruthy();
+    });
+
+    it('does not overwrite the ID of the provided circle element', () => {
+      let circle = new SVG.Circle();
+      circle.attr('id', 'id-1274618246');
+
+      let outline = new CircleBaseOutline({ circle });
+      expect(circle.attr('id')).toBe('id-1274618246');
+    });
+
+    it('initializes the ID when creating a new circle element', () => {
+      let outline = new CircleBaseOutline();
+
+      expect(outline.circle.attr('id')).toMatch(uuidRegex);
+      expect(outline.circle.attr('id').length).toBeGreaterThanOrEqual(36);
+
+      // must start with a letter in SVG
+      expect(outline.circle.attr('id').startsWith('uuid-')).toBeTruthy();
     });
   });
 });
--- src/draw/bases/annotate/circle/private/CircleBaseOutline.ts
+++ src/draw/bases/annotate/circle/private/CircleBaseOutline.ts
@@ -1,5 +1,7 @@
 import * as SVG from '@svgdotjs/svg.js';
 
+import { v4 as uuidv4 } from 'uuid';
+
 export type Point = {
   x: number;
   y: number;
@@ -30,8 +32,23 @@ export class CircleBaseOutline {
    */
   cachedBaseCenter?: Point;
 
+  /**
+   * Will initialize the ID of the provided circle element with a UUID
+   * if it does not already have an ID.
+   *
+   * (The circle element of a circle base outline must have a unique ID
+   * for the circle element to be retrieved when recreating a circle
+   * base outline, such as when opening a saved drawing.)
+   */
   constructor(args?: ConstructorArgs) {
     this.circle = args?.circle ?? new SVG.Circle();
+
+    // use the attr method instead of the id method
+    // (the id method will initialize the ID to a non-UUID)
+    if (!this.circle.attr('id')) {
+      let id = 'uuid-' + uuidv4(); // must start with a letter in SVG
+      this.circle.attr('id', id);
+    }
 
     this.cachedBaseCenter = args?.baseCenter;
   }
