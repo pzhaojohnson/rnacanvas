92a9542a0120ee14518fbdc9beaf16d47eb9a226
Philip Johnson <p.zhao.johnson@gmail.com>
2023-02-17 17:32:55 -0500

    Stop updating default fill value in FillPicker component

--- src/forms/edit/bases/outlines/FillPicker.tsx
+++ src/forms/edit/bases/outlines/FillPicker.tsx
@@ -1,21 +1,10 @@
 import type { App } from 'App';
 
-import { CircleBaseAnnotation as BaseOutline } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
-
-import * as SVG from '@svgdotjs/svg.js';
+import type { CircleBaseAnnotation as BaseOutline } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
 
 import * as React from 'react';
 
 import { ColorAttributePicker } from 'Forms/edit/svg/ColorAttributePicker';
-
-import { colorValuesAreEqual } from 'Draw/svg/colorValuesAreEqual';
-
-/**
- * Returns true if the color is white.
- */
-function isWhite(color: SVG.Color): boolean {
-  return colorValuesAreEqual(color.toHex(), '#ffffff');
-}
 
 export type Props = {
   /**
@@ -38,13 +27,6 @@ export function FillPicker(props: Props) {
         props.app.pushUndo();
       }}
       onEdit={event => {
-        let newValue = event.newValue;
-
-        // don't make the same color as the background by default
-        if (!isWhite(newValue)) {
-          BaseOutline.recommendedDefaults.circle['fill'] = newValue.toHex();
-        }
-
         props.app.refresh(); // refresh after updating all values
       }}
     />
