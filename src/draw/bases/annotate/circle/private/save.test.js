961ae253ddb45398d4e2bfa9daad8ae83be9af5a
Philip Johnson <p.zhao.johnson@gmail.com>
2023-02-17 16:32:08 -0500

    Add fromSaved static method

--- src/draw/bases/annotate/circle/private/save.test.js
+++ src/draw/bases/annotate/circle/private/save.test.js
@@ -2,6 +2,8 @@ import { CircleBaseOutline } from './CircleBaseOutline';
 
 import * as SVG from 'Draw/svg/NodeSVG';
 
+import { Circle as SVGCircle } from '@svgdotjs/svg.js';
+
 import { CircleBaseOutlineDecorator } from './save';
 
 let svg = null;
@@ -31,6 +33,76 @@ afterEach(() => {
 });
 
 describe('CircleBaseOutlineDecorator class', () => {
+  describe('fromSaved static method', () => {
+    it('finds circle element', () => {
+      // add extra elements to sift through
+      svg.text('A');
+      svg.circle(25).back();
+      svg.circle(10);
+      svg.rect(10, 20).back();
+      svg.text('g');
+
+      let saved = circleBaseOutlineDecorator.toSaved();
+
+      let circleBaseOutline2 = (
+        CircleBaseOutlineDecorator.fromSaved({ saved, parent: svg })
+      );
+
+      expect(circleBaseOutline2.circle).toBeInstanceOf(SVGCircle);
+      expect(circleBaseOutline2.circle.attr('id')).toBe(saved.circleId);
+      expect(circleBaseOutline2.circle.attr('id')).toBeTruthy();
+    });
+
+    test('missing circle ID', () => {
+      let saved = circleBaseOutlineDecorator.toSaved();
+      saved.circleId = undefined;
+
+      expect(
+        () => CircleBaseOutlineDecorator.fromSaved({ saved, parent: svg })
+      ).toThrow();
+    });
+
+    test('empty circle ID', () => {
+      let saved = circleBaseOutlineDecorator.toSaved();
+      saved.circleId = '';
+
+      expect(
+        () => CircleBaseOutlineDecorator.fromSaved({ saved, parent: svg })
+      ).toThrow();
+    });
+
+    test('missing circle element', () => {
+      let saved = circleBaseOutlineDecorator.toSaved();
+      circleBaseOutline.circle.remove();
+
+      expect(
+        () => CircleBaseOutlineDecorator.fromSaved({ saved, parent: svg })
+      ).toThrow();
+    });
+
+    test('nonunique circle ID', () => {
+      circleBaseOutline.circle.attr('id', 'asdf-12345');
+
+      let circle2 = svg.circle(5);
+      circle2.attr('id', 'asdf-12345');
+
+      let saved = circleBaseOutlineDecorator.toSaved();
+
+      // does not currently throw
+      expect(
+        () => CircleBaseOutlineDecorator.fromSaved({ saved, parent: svg })
+      ).not.toThrow();
+    });
+
+    test('unexpected saved types', () => {
+      [undefined, null, {}, 2, true, 'asdf'].forEach(saved => {
+        expect(
+          () => CircleBaseOutlineDecorator.fromSaved({ saved, parent: svg })
+        ).toThrow();
+      });
+    });
+  });
+
   describe('save method', () => {
     it('includes class name and circle ID', () => {
       circleBaseOutline.circle.attr('id', 'id-1274618abhc');
--- src/draw/bases/annotate/circle/private/save.ts
+++ src/draw/bases/annotate/circle/private/save.ts
@@ -1,5 +1,7 @@
 import { CircleBaseOutline } from './CircleBaseOutline';
 
+import * as SVG from '@svgdotjs/svg.js';
+
 /**
  * Includes legacy class name.
  */
@@ -22,7 +24,49 @@ export type SavedCircleBaseOutline = {
   circleId: string;
 };
 
+export type FromSavedStaticMethodParameters = {
+  /**
+   * Meant to be a saved circle base outline, but could be anything
+   * since could be read from a file.
+   */
+  saved: SavedCircleBaseOutline | unknown;
+
+  /**
+   * The SVG document that the saved circle base outline is in.
+   */
+  parent: SVG.Svg;
+};
+
 export class CircleBaseOutlineDecorator {
+  /**
+   * Throws if the saved circle base outline is invalid.
+   */
+  static fromSaved
+  (
+    args: FromSavedStaticMethodParameters,
+  ): CircleBaseOutline | never
+  {
+    let { saved, parent } = args;
+
+    let circleId: string | unknown = undefined;
+    let circle: SVG.Circle | unknown = undefined;
+
+    try {
+      circleId = (saved as any).circleId;
+      circle = parent.findOne('#' + (circleId as any));
+    } catch {}
+
+    if (typeof circleId != 'string') {
+      throw new Error('Missing circle ID.');
+    } else if (!circleId) {
+      throw new Error('Missing circle ID.');
+    } else if (!(circle instanceof SVG.Circle)) {
+      throw new Error('Unable to find circle element.');
+    }
+
+    return new CircleBaseOutline({ circle });
+  }
+
   readonly decoratedCircleBaseOutline: CircleBaseOutline;
 
   constructor(circleBaseOutline: CircleBaseOutline) {
