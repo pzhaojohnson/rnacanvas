20ef752ee9640a276be363768a6e92fd5ae74994
Philip Johnson <p.zhao.johnson@gmail.com>
2023-02-22 08:32:18 -0500

    Stop updating default stroke opacity in StrokeOpacityInput for base outlines

--- src/forms/edit/bases/outlines/StrokeOpacityInput.tsx
+++ src/forms/edit/bases/outlines/StrokeOpacityInput.tsx
@@ -1,6 +1,6 @@
 import type { App } from 'App';
 
-import { CircleBaseAnnotation as BaseOutline } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
+import type { CircleBaseAnnotation as BaseOutline } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
 
 import * as React from 'react';
 
@@ -31,10 +31,6 @@ export class StrokeOpacityInput extends React.Component<Props> {
   }
 
   handleEdit(event: EditEvent) {
-    let newValue = event.newValue;
-
-    BaseOutline.recommendedDefaults.circle['stroke-opacity'] = newValue;
-
     this.props.app.refresh(); // refresh after updating all values
   }
 
