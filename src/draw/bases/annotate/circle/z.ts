b7647d43cf3d1fa096208e808414f47079aa2cc0
Philip Johnson <p.zhao.johnson@gmail.com>
2023-02-22 20:44:46 -0500

    Remove unused z module

--- src/draw/bases/annotate/circle/z.ts
+++
@@ -1,9 +0,0 @@
-import type { CircleBaseAnnotation } from './CircleBaseAnnotation';
-
-export function bringToFront(cba: CircleBaseAnnotation) {
-  cba.circle.front();
-}
-
-export function sendToBack(cba: CircleBaseAnnotation) {
-  cba.circle.back();
-}
