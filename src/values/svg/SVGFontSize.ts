1baa4af8e059b0d14cd8bba6b9414fa4f6761865
Philip Johnson <p.zhao.johnson@gmail.com>
2023-02-27 19:39:37 -0500

    Add SVGFontSize class

---
+++ src/values/svg/SVGFontSize.ts
@@ -0,0 +1,9 @@
+import { PositiveFiniteNumber } from 'Values/PositiveFiniteNumber';
+
+export {
+  /**
+   * Currently the SVGFontSize class is just an alias for the
+   * PositiveFiniteNumber class.
+   */
+  PositiveFiniteNumber as SVGFontSize,
+};
