f92ee51b4c27ff77793d525887e37e50de2a9b10
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-03 16:57:08 -0500

    Move comment and convert to doc comment

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,9 +1,11 @@
 export class Settings {
+  /**
+   * Specifies whether or not a prompt should be displayed before
+   * the user leaves warning about possibly losing unsaved work.
+   */
   askBeforeLeaving: boolean;
 
   constructor() {
-    // specifies whether or not a prompt should be displayed before
-    // the user leaves warning about possibly losing unsaved work
     this.askBeforeLeaving = true;
   }
 }
