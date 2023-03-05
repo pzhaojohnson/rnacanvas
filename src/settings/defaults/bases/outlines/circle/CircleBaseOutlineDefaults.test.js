e0fb0dc2e51e2f266051fdcdd35ea934c721fdc2
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-05 11:35:43 -0500

    Add CircleBaseOutlineDefaults class

---
+++ src/settings/defaults/bases/outlines/circle/CircleBaseOutlineDefaults.test.js
@@ -0,0 +1,8 @@
+import { CircleBaseOutlineDefaults } from './CircleBaseOutlineDefaults';
+
+describe('CircleBaseOutlineDefaults class', () => {
+  test('constructor', () => {
+    expect(() => new CircleBaseOutlineDefaults())
+      .not.toThrow();
+  });
+});
---
+++ src/settings/defaults/bases/outlines/circle/CircleBaseOutlineDefaults.ts
@@ -0,0 +1,40 @@
+import { NonNegativeFiniteNumber } from 'Values/NonNegativeFiniteNumber';
+
+import { SVGColor } from 'Values/svg/SVGColor';
+import { SVGOpacity } from 'Values/svg/SVGOpacity';
+
+const circleAttributeNames = [
+  'r',
+  'stroke',
+  'stroke-width',
+  'stroke-opacity',
+  'fill',
+  'fill-opacity',
+] as const;
+
+type CircleAttributeName = typeof circleAttributeNames[number];
+
+/**
+ * Default values for circle base outlines in the drawing of the app.
+ */
+export class CircleBaseOutlineDefaults {
+  circle: {
+    'r': NonNegativeFiniteNumber,
+    'stroke': SVGColor,
+    'stroke-width': NonNegativeFiniteNumber,
+    'stroke-opacity': SVGOpacity,
+    'fill': SVGColor,
+    'fill-opacity': SVGOpacity,
+  };
+
+  constructor() {
+    this.circle = {
+      'r': new NonNegativeFiniteNumber(7),
+      'stroke': new SVGColor('#00ffff'),
+      'stroke-width': new NonNegativeFiniteNumber(1),
+      'stroke-opacity': new SVGOpacity(1),
+      'fill': new SVGColor('#c3ffff'),
+      'fill-opacity': new SVGOpacity(1),
+    };
+  }
+}
