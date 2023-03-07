212ed7c865c8b648a1a9b3247c4f6c539be12072
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-07 14:13:54 -0500

    Add applyTo, toSaved and applySaved methods

--- src/settings/defaults/bases/numberings/BaseNumberingDefaults.test.js
+++ src/settings/defaults/bases/numberings/BaseNumberingDefaults.test.js
@@ -1,8 +1,253 @@
+import { BaseNumbering } from 'Draw/bases/numberings/BaseNumbering';
+
+import * as SVG from 'Draw/svg/NodeSVG';
+
+import { Color as SVGColor } from '@svgdotjs/svg.js';
+
 import { BaseNumberingDefaults } from './BaseNumberingDefaults';
+
+function createBaseNumbering() {
+  let baseCenter = { x: 20, y: 60 };
+
+  let text = svg.text('109');
+  text.center(30, 80);
+
+  let line = svg.line(21, 62, 29, 78);
+
+  return new BaseNumbering(text, line, baseCenter);
+}
+
+const someFontFamilies = [
+  'Georgia, serif',
+  '"Goudy Bookletter 1911", sans-serif',
+  '"Lucida Console", Courier, monospace',
+  '"Comic Sans", sans-serif',
+  'cursive',
+  'system-ui',
+];
+
+/**
+ * Randomly picks one item from the array.
+ */
+function pickOneRandom(array) {
+  let i = Math.floor(
+    array.length * Math.random()
+  );
+
+  return array[i];
+}
+
+let svg = null;
+
+let baseNumbering = null;
+
+let randomTextAttributes = null;
+let randomLineAttributes = null;
+let randomProperties = null;
+
+let randomDefaults = null;
+
+beforeEach(() => {
+  svg = SVG.SVG();
+  svg.addTo(document.body);
+
+  baseNumbering = createBaseNumbering();
+
+  randomTextAttributes = [
+    ['font-family', pickOneRandom(someFontFamilies)],
+    ['font-size', 18 * Math.random()],
+    ['font-weight', 100 + (800 * Math.random())],
+    ['fill', SVGColor.random().toHex()],
+    ['fill-opacity', Math.random()],
+  ];
+
+  randomLineAttributes = [
+    ['stroke', SVGColor.random().toHex()],
+    ['stroke-width', 20 * Math.random()],
+    ['stroke-opacity', Math.random()],
+  ];
+
+  randomProperties = [
+    ['basePadding', 30 * Math.random()],
+    ['lineLength', 25 * Math.random()],
+  ];
+
+  randomDefaults = new BaseNumberingDefaults();
+
+  randomTextAttributes
+    .forEach(a => randomDefaults.text[a[0]].setValue(a[1]));
+
+  randomLineAttributes
+    .forEach(a => randomDefaults.line[a[0]].setValue(a[1]));
+
+  randomProperties
+    .forEach(p => randomDefaults[p[0]].setValue(p[1]));
+});
+
+afterEach(() => {
+  randomDefaults = null;
+
+  randomTextAttributes = null;
+  randomLineAttributes = null;
+  randomProperties = null;
+
+  baseNumbering = null;
+
+  svg.remove();
+  svg = null;
+});
 
 describe('BaseNumberingDefaults class', () => {
   test('constructor', () => {
     expect(() => new BaseNumberingDefaults())
       .not.toThrow();
+  });
+
+  describe('applyTo method', () => {
+    beforeEach(() => {
+      randomDefaults.applyTo(baseNumbering);
+    });
+
+    it('applies text attributes', () => {
+      randomTextAttributes
+        .forEach(a => expect(baseNumbering.text.attr(a[0])).toBe(a[1]));
+    });
+
+    it('applies line attributes', () => {
+      randomLineAttributes
+        .forEach(a => expect(baseNumbering.line.attr(a[0])).toBe(a[1]));
+    });
+
+    it('applies properties', () => {
+      randomProperties
+        .forEach(p => expect(baseNumbering[p[0]]).toBeCloseTo(p[1]));
+    });
+  });
+
+  describe('toSaved method', () => {
+    let saved = null;
+
+    beforeEach(() => {
+      saved = randomDefaults.toSaved();
+    });
+
+    it('includes text attributes', () => {
+      randomTextAttributes
+        .forEach(a => expect(saved.text[a[0]]).toBe(a[1]));
+    });
+
+    it('includes line attributes', () => {
+      randomLineAttributes
+        .forEach(a => expect(saved.line[a[0]]).toBe(a[1]));
+    });
+
+    it('includes properties', () => {
+      randomProperties
+        .forEach(p => expect(saved[p[0]]).toBeCloseTo(p[1]));
+    });
+
+    test('JSON conversion', () => {
+      let json = JSON.stringify(saved);
+      expect(JSON.parse(json)).toStrictEqual(saved);
+    });
+  });
+
+  describe('applySaved method', () => {
+    describe('applying saved values', () => {
+      let defaults = null;
+
+      beforeEach(() => {
+        defaults = new BaseNumberingDefaults();
+
+        let saved = randomDefaults.toSaved();
+        defaults.applySaved(saved);
+      });
+
+      it('applies saved text attributes', () => {
+        randomTextAttributes
+          .forEach(a => expect(defaults.text[a[0]].getValue()).toBe(a[1]));
+      });
+
+      it('applies saved line attributes', () => {
+        randomLineAttributes
+          .forEach(a => expect(defaults.line[a[0]].getValue()).toBe(a[1]));
+      });
+
+      it('applies saved properties', () => {
+        randomProperties
+          .forEach(p => expect(defaults[p[0]].getValue()).toBeCloseTo(p[1]));
+      });
+    });
+
+    describe('ignoring invalid saved values', () => {
+      beforeEach(() => {
+        // all values are invalid
+        randomDefaults.applySaved({
+          text: {
+            'font-family': 2,
+            'font-size': 'asdf',
+            'font-weight': false,
+            'fill': 'zxcv',
+            'fill-opacity': 'AAA',
+          },
+          line: {
+            'stroke': 8,
+            'stroke-width': 'Q',
+            'stroke-opacity': {},
+          },
+          basePadding: 'soifeg',
+          lineLength: false,
+        });
+      });
+
+      it('ignores invalid saved text attributes', () => {
+        randomTextAttributes.forEach(a => {
+          expect(randomDefaults.text[a[0]].getValue()).toBe(a[1]);
+        });
+      });
+
+      it('ignores invalid saved line attributes', () => {
+        randomLineAttributes.forEach(a => {
+          expect(randomDefaults.line[a[0]].getValue()).toBe(a[1]);
+        });
+      });
+
+      it('ignores invalid saved properties', () => {
+        randomProperties.forEach(p => {
+          expect(randomDefaults[p[0]].getValue()).toBeCloseTo(p[1]);
+        });
+      });
+    });
+
+    describe('ignoring undefined saved values', () => {
+      beforeEach(() => {
+        // with defined text and line objects
+        randomDefaults.applySaved({ text: {}, line: {} });
+
+        // with undefined text and line objects
+        randomDefaults.applySaved({});
+
+        // simply a value of undefined
+        randomDefaults.applySaved(undefined);
+      });
+
+      it('ignores undefined saved text attributes', () => {
+        randomTextAttributes.forEach(a => {
+          expect(randomDefaults.text[a[0]].getValue()).toBe(a[1]);
+        });
+      });
+
+      it('ignores undefined saved line attributes', () => {
+        randomLineAttributes.forEach(a => {
+          expect(randomDefaults.line[a[0]].getValue()).toBe(a[1]);
+        });
+      });
+
+      it('ignores undefined saved properties', () => {
+        randomProperties.forEach(p => {
+          expect(randomDefaults[p[0]].getValue()).toBeCloseTo(p[1]);
+        });
+      });
+    });
   });
 });
--- src/settings/defaults/bases/numberings/BaseNumberingDefaults.ts
+++ src/settings/defaults/bases/numberings/BaseNumberingDefaults.ts
@@ -1,3 +1,5 @@
+import type { BaseNumbering } from 'Draw/bases/numberings/BaseNumbering';
+
 import { NonNegativeFiniteNumber } from 'Values/NonNegativeFiniteNumber';
 
 import { SVGColor } from 'Values/svg/SVGColor';
@@ -25,6 +27,16 @@ const propertyNames = [
   'basePadding',
   'lineLength',
 ] as const;
+
+export type SavedBaseNumberingDefaults = (
+  ReturnType<
+    InstanceType<typeof BaseNumberingDefaults>['toSaved']
+  >
+);
+
+type SavedTextAttributes = SavedBaseNumberingDefaults['text'];
+
+type SavedLineAttributes = SavedBaseNumberingDefaults['line'];
 
 /**
  * Default values for base numberings in the drawing of the app.
@@ -64,5 +76,111 @@ export class BaseNumberingDefaults {
 
     this.basePadding = new NonNegativeFiniteNumber(8);
     this.lineLength = new NonNegativeFiniteNumber(8);
+  }
+
+  /**
+   * Sets the values of the base numbering to these default values.
+   */
+  applyTo(bn: BaseNumbering) {
+    textAttributeNames.forEach(name => {
+      bn.text.attr(name, this.text[name].getValue());
+    });
+
+    lineAttributeNames.forEach(name => {
+      bn.line.attr(name, this.line[name].getValue());
+    });
+
+    propertyNames.forEach(name => {
+      bn[name] = this[name].getValue();
+    });
+  }
+
+  /**
+   * Returns the saved form of these base numbering defaults.
+   *
+   * The saved form of these base numbering defaults can be directly
+   * converted to and from JSON.
+   */
+  toSaved() {
+    return {
+      text: {
+        'font-family': this.text['font-family'].getValue(),
+        'font-size': this.text['font-size'].getValue(),
+        'font-weight': this.text['font-weight'].getValue(),
+        'fill': this.text['fill'].getValue(),
+        'fill-opacity': this.text['fill-opacity'].getValue(),
+      },
+      line: {
+        'stroke': this.line['stroke'].getValue(),
+        'stroke-width': this.line['stroke-width'].getValue(),
+        'stroke-opacity': this.line['stroke-opacity'].getValue(),
+      },
+      basePadding: this.basePadding.getValue(),
+      lineLength: this.lineLength.getValue(),
+    };
+  }
+
+  /**
+   * Sets the values of these base numbering defaults to the saved
+   * default values.
+   */
+  applySaved(saved: SavedBaseNumberingDefaults): void;
+
+  /**
+   * Since the saved default values might have been read from a file,
+   * this method is designed to handle any unknown saved value(s).
+   *
+   * Invalid and undefined saved values are ignored.
+   */
+  applySaved(saved: unknown): void;
+
+  applySaved(saved: unknown) {
+    try {
+      this._applySavedTextAttributes((saved as any).text);
+    } catch {}
+
+    try {
+      this._applySavedLineAttributes((saved as any).line);
+    } catch {}
+
+    try {
+      this._applySavedProperties(saved);
+    } catch {}
+  }
+
+  _applySavedTextAttributes(saved: SavedTextAttributes | unknown) {
+    textAttributeNames.forEach(name => {
+      try {
+        let value: unknown = (saved as any)[name];
+
+        if (value !== undefined) {
+          this.text[name].setValue(value);
+        }
+      } catch {}
+    });
+  }
+
+  _applySavedLineAttributes(saved: SavedLineAttributes | unknown) {
+    lineAttributeNames.forEach(name => {
+      try {
+        let value: unknown = (saved as any)[name];
+
+        if (value !== undefined) {
+          this.line[name].setValue(value);
+        }
+      } catch {}
+    });
+  }
+
+  _applySavedProperties(saved: SavedBaseNumberingDefaults | unknown) {
+    propertyNames.forEach(name => {
+      try {
+        let value: unknown = (saved as any)[name];
+
+        if (value !== undefined) {
+          this[name].setValue(value);
+        }
+      } catch {}
+    });
   }
 }
