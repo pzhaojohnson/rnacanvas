fa61dcef6ecfac25d7c7b6f4634641a6b2fafd95
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-08 08:09:14 -0500

    Add applyTo method

--- src/settings/defaults/bonds/primary/PrimaryBondDefaults.test.js
+++ src/settings/defaults/bonds/primary/PrimaryBondDefaults.test.js
@@ -1,8 +1,105 @@
+import { PrimaryBond } from 'Draw/bonds/straight/PrimaryBond';
+
+import { Base } from 'Draw/bases/Base';
+
+import * as SVG from 'Draw/svg/NodeSVG';
+
+import { Color as SVGColor } from '@svgdotjs/svg.js';
+
 import { PrimaryBondDefaults } from './PrimaryBondDefaults';
+
+let svg = null;
+
+let primaryBond = null;
+
+let randomLineAttributes = null;
+let randomProperties = null;
+
+let randomDefaults = null;
+
+beforeEach(() => {
+  svg = SVG.SVG();
+  svg.addTo(document.body);
+});
+
+/**
+ * Create a primary bond.
+ */
+beforeEach(() => {
+  let base1 = new Base({ text: svg.text('A') });
+  let base2 = new Base({ text: svg.text('U') });
+
+  base1.recenter({ x: 10, y: 20 });
+  base2.recenter({ x: 30, y: 50 });
+
+  let line = svg.line(12, 23, 28, 47);
+
+  primaryBond = new PrimaryBond(line, base1, base2);
+});
+
+/**
+ * Generate some random line attributes and properties.
+ */
+beforeEach(() => {
+  randomLineAttributes = [
+    ['stroke', SVGColor.random().toHex()],
+    ['stroke-width', 20 * Math.random()],
+    ['stroke-opacity', Math.random()],
+  ];
+
+  randomProperties = [
+    ['basePadding1', 9 * Math.random()],
+    ['basePadding2', 6 * Math.random()],
+  ];
+});
+
+/**
+ * Create a random defaults object and assign it the random line
+ * attributes and properties generated beforehand.
+ */
+beforeEach(() => {
+  randomDefaults = new PrimaryBondDefaults();
+
+  randomLineAttributes
+    .forEach(a => randomDefaults.line[a[0]].setValue(a[1]));
+
+  randomProperties
+    .forEach(p => randomDefaults[p[0]].setValue(p[1]));
+});
+
+afterEach(() => {
+  randomDefaults = null;
+
+  randomLineAttributes = null;
+  randomProperties = null;
+
+  primaryBond = null;
+
+  svg.remove();
+  svg = null;
+});
 
 describe('PrimaryBondDefaults class', () => {
   test('constructor', () => {
     expect(() => new PrimaryBondDefaults())
       .not.toThrow();
+  });
+
+  describe('applyTo method', () => {
+    beforeEach(() => {
+      randomDefaults.applyTo(primaryBond);
+    });
+
+    it('applies line attributes', () => {
+      randomLineAttributes.forEach(a => {
+        expect(primaryBond.line.attr(a[0])).toBe(a[1]);
+      });
+    });
+
+    it('applies properties', () => {
+      randomProperties.forEach(p => {
+        expect(primaryBond[p[0]]).toBeCloseTo(p[1]);
+      });
+    });
   });
 });
--- src/settings/defaults/bonds/primary/PrimaryBondDefaults.ts
+++ src/settings/defaults/bonds/primary/PrimaryBondDefaults.ts
@@ -1,7 +1,20 @@
+import type { PrimaryBond } from 'Draw/bonds/straight/PrimaryBond';
+
 import { NonNegativeFiniteNumber } from 'Values/NonNegativeFiniteNumber';
 
 import { SVGColor } from 'Values/svg/SVGColor';
 import { SVGOpacity } from 'Values/svg/SVGOpacity';
+
+const lineAttributeNames = [
+  'stroke',
+  'stroke-width',
+  'stroke-opacity',
+] as const;
+
+const propertyNames = [
+  'basePadding1',
+  'basePadding2',
+] as const;
 
 /**
  * Default values for primary bonds in the drawing of the app.
@@ -25,5 +38,18 @@ export class PrimaryBondDefaults {
 
     this.basePadding1 = new NonNegativeFiniteNumber(8);
     this.basePadding2 = new NonNegativeFiniteNumber(8);
+  }
+
+  /**
+   * Sets the values of the primary bond to these default values.
+   */
+  applyTo(pb: PrimaryBond) {
+    lineAttributeNames.forEach(name => {
+      pb.line.attr(name, this.line[name].getValue());
+    });
+
+    propertyNames.forEach(name => {
+      pb[name] = this[name].getValue();
+    });
   }
 }
