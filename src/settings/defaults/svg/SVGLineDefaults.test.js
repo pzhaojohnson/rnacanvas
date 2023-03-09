062abaa62f52a02c5c1feea02b6fc4fc147de964
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-09 16:36:28 -0500

    Add applyTo method

--- src/settings/defaults/svg/SVGLineDefaults.test.js
+++ src/settings/defaults/svg/SVGLineDefaults.test.js
@@ -1,6 +1,41 @@
+import * as SVG from 'Draw/svg/NodeSVG';
+
 import { SVGLineDefaults } from './SVGLineDefaults';
 
-test('SVGLineDefaults class', () => {
-  expect(() => new SVGLineDefaults())
-    .not.toThrow();
+let svg = null;
+
+let line = null;
+
+let defaults = null;
+
+beforeEach(() => {
+  svg = SVG.SVG();
+  svg.addTo(document.body);
+
+  line = svg.line(10, 100, 35, 678);
+
+  defaults = new SVGLineDefaults();
+});
+
+afterEach(() => {
+  defaults = null;
+
+  line = null;
+
+  svg.remove();
+  svg = null;
+});
+
+describe('SVGLineDefaults class', () => {
+  test('applyTo method', () => {
+    defaults['stroke'].setValue('#55abc2');
+    defaults['stroke-width'].setValue(8.043);
+    defaults['stroke-opacity'].setValue(0.1903);
+
+    defaults.applyTo(line);
+
+    expect(line.attr('stroke')).toBe('#55abc2');
+    expect(line.attr('stroke-width')).toBe(8.043);
+    expect(line.attr('stroke-opacity')).toBe(0.1903);
+  });
 });
--- src/settings/defaults/svg/SVGLineDefaults.ts
+++ src/settings/defaults/svg/SVGLineDefaults.ts
@@ -1,7 +1,15 @@
+import * as SVG from '@svgdotjs/svg.js';
+
 import { NonNegativeFiniteNumber } from 'Values/NonNegativeFiniteNumber';
 
 import { SVGColor } from 'Values/svg/SVGColor';
 import { SVGOpacity } from 'Values/svg/SVGOpacity';
+
+const attributeNames = [
+  'stroke',
+  'stroke-width',
+  'stroke-opacity',
+] as const;
 
 /**
  * Default values for SVG line elements in the drawing of the app.
@@ -10,4 +18,13 @@ export class SVGLineDefaults {
   'stroke' = new SVGColor('#000000');
   'stroke-width' = new NonNegativeFiniteNumber(1);
   'stroke-opacity' = new SVGOpacity(1);
+
+  /**
+   * Sets the values of the SVG line element to these default values.
+   */
+  applyTo(line: SVG.Line) {
+    attributeNames.forEach(name => {
+      line.attr(name, this[name].getValue());
+    });
+  }
 }
