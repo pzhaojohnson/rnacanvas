b7f316067e9a8bc9663ff3e182173e1ea9251eae
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-12 17:01:24 -0400

    Use StraightBondDefaults class in SecondaryBondDefaults class

--- src/settings/defaults/bonds/SecondaryBondDefaults.ts
+++ src/settings/defaults/bonds/SecondaryBondDefaults.ts
@@ -1,50 +1,38 @@
-import { NonNegativeFiniteNumber } from 'Values/NonNegativeFiniteNumber';
-
-import { SVGColor } from 'Values/svg/SVGColor';
-import { SVGOpacity } from 'Values/svg/SVGOpacity';
+import { StraightBondDefaults } from 'Settings/defaults/bonds/StraightBondDefaults';
 
 /**
  * Default values for secondary bonds in the drawing of the app
  * organized by secondary bond type (e.g., AUT, GC, GUT, other).
  */
 export class SecondaryBondDefaults {
-  AUT = {
-    line: {
-      'stroke': new SVGColor('#000000'),
-      'stroke-width': new NonNegativeFiniteNumber(2),
-      'stroke-opacity': new SVGOpacity(1),
-    },
-    basePadding1: new NonNegativeFiniteNumber(6),
-    basePadding2: new NonNegativeFiniteNumber(6),
-  };
+  AUT = new StraightBondDefaults();
+  GC = new StraightBondDefaults();
+  GUT = new StraightBondDefaults();
+  other = new StraightBondDefaults();
 
-  GC = {
-    line: {
-      'stroke': new SVGColor('#000000'),
-      'stroke-width': new NonNegativeFiniteNumber(2),
-      'stroke-opacity': new SVGOpacity(1),
-    },
-    basePadding1: new NonNegativeFiniteNumber(6),
-    basePadding2: new NonNegativeFiniteNumber(6),
-  };
+  constructor() {
+    this.AUT.line['stroke'].setValue('#000000');
+    this.AUT.line['stroke-width'].setValue(2);
+    this.AUT.line['stroke-opacity'].setValue(1);
+    this.AUT.basePadding1.setValue(6);
+    this.AUT.basePadding2.setValue(6);
 
-  GUT = {
-    line: {
-      'stroke': new SVGColor('#000000'),
-      'stroke-width': new NonNegativeFiniteNumber(2),
-      'stroke-opacity': new SVGOpacity(1),
-    },
-    basePadding1: new NonNegativeFiniteNumber(6),
-    basePadding2: new NonNegativeFiniteNumber(6),
-  };
+    this.GC.line['stroke'].setValue('#000000');
+    this.GC.line['stroke-width'].setValue(2);
+    this.GC.line['stroke-opacity'].setValue(1);
+    this.GC.basePadding1.setValue(6);
+    this.GC.basePadding2.setValue(6);
 
-  other = {
-    line: {
-      'stroke': new SVGColor('#000000'),
-      'stroke-width': new NonNegativeFiniteNumber(2),
-      'stroke-opacity': new SVGOpacity(1),
-    },
-    basePadding1: new NonNegativeFiniteNumber(6),
-    basePadding2: new NonNegativeFiniteNumber(6),
-  };
+    this.GUT.line['stroke'].setValue('#000000');
+    this.GUT.line['stroke-width'].setValue(2);
+    this.GUT.line['stroke-opacity'].setValue(1);
+    this.GUT.basePadding1.setValue(6);
+    this.GUT.basePadding2.setValue(6);
+
+    this.other.line['stroke'].setValue('#000000');
+    this.other.line['stroke-width'].setValue(2);
+    this.other.line['stroke-opacity'].setValue(1);
+    this.other.basePadding1.setValue(6);
+    this.other.basePadding2.setValue(6);
+  }
 }
