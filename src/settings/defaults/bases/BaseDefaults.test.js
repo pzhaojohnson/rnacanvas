ca80c8c38525534a79d37068c50a9a5e76f9c6aa
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-12 13:13:38 -0400

    Refactor baseDefaults variable to defaults

--- src/settings/defaults/bases/BaseDefaults.test.js
+++ src/settings/defaults/bases/BaseDefaults.test.js
@@ -8,7 +8,7 @@ let svg = null;
 
 let base = null;
 
-let baseDefaults = null;
+let defaults = null;
 
 beforeEach(() => {
   svg = SVG.SVG();
@@ -18,11 +18,11 @@ beforeEach(() => {
     text: svg.text('A'),
   });
 
-  baseDefaults = new BaseDefaults();
+  defaults = new BaseDefaults();
 });
 
 afterEach(() => {
-  baseDefaults = null;
+  defaults = null;
 
   base = null;
 
@@ -37,12 +37,12 @@ describe('BaseDefaults class', () => {
   });
 
   test('applyTo method', () => {
-    baseDefaults.text['font-family'].setValue('Courier New');
-    baseDefaults.text['font-size'].setValue(29.8);
-    baseDefaults.text['font-weight'].setValue(312.9);
-    baseDefaults.text['font-style'].setValue('oblique');
+    defaults.text['font-family'].setValue('Courier New');
+    defaults.text['font-size'].setValue(29.8);
+    defaults.text['font-weight'].setValue(312.9);
+    defaults.text['font-style'].setValue('oblique');
 
-    baseDefaults.applyTo(base);
+    defaults.applyTo(base);
     expect(base.text.attr('font-family')).toBe('Courier New');
     expect(base.text.attr('font-size')).toBe(29.8);
     expect(base.text.attr('font-weight')).toBe(312.9);
@@ -50,12 +50,12 @@ describe('BaseDefaults class', () => {
   });
 
   test('toSaved method', () => {
-    baseDefaults.text['font-family'].setValue('Consolas');
-    baseDefaults.text['font-size'].setValue(4.89);
-    baseDefaults.text['font-weight'].setValue(709.1);
-    baseDefaults.text['font-style'].setValue('italic');
+    defaults.text['font-family'].setValue('Consolas');
+    defaults.text['font-size'].setValue(4.89);
+    defaults.text['font-weight'].setValue(709.1);
+    defaults.text['font-style'].setValue('italic');
 
-    let saved = baseDefaults.toSaved();
+    let saved = defaults.toSaved();
 
     expect(saved.text['font-family']).toBe('Consolas');
     expect(saved.text['font-size']).toBe(4.89);
@@ -69,55 +69,55 @@ describe('BaseDefaults class', () => {
 
   describe('applySaved method', () => {
     it('applies saved values', () => {
-      baseDefaults.text['font-family'].setValue('Arial');
-      baseDefaults.text['font-size'].setValue(9);
-      baseDefaults.text['font-weight'].setValue(700);
-      baseDefaults.text['font-style'].setValue('normal');
+      defaults.text['font-family'].setValue('Arial');
+      defaults.text['font-size'].setValue(9);
+      defaults.text['font-weight'].setValue(700);
+      defaults.text['font-style'].setValue('normal');
 
-      let saved = baseDefaults.toSaved();
+      let saved = defaults.toSaved();
       saved.text['font-family'] = '"Gill Sans"';
       saved.text['font-size'] = 112.05;
       saved.text['font-weight'] = 802.55;
       saved.text['font-style'] = 'italic';
 
-      baseDefaults.applySaved(saved);
-      expect(baseDefaults.text['font-family'].getValue()).toBe('"Gill Sans"');
-      expect(baseDefaults.text['font-size'].getValue()).toBe(112.05);
-      expect(baseDefaults.text['font-weight'].getValue()).toBe(802.55);
-      expect(baseDefaults.text['font-style'].getValue()).toBe('italic');
+      defaults.applySaved(saved);
+      expect(defaults.text['font-family'].getValue()).toBe('"Gill Sans"');
+      expect(defaults.text['font-size'].getValue()).toBe(112.05);
+      expect(defaults.text['font-weight'].getValue()).toBe(802.55);
+      expect(defaults.text['font-style'].getValue()).toBe('italic');
     });
 
     it('ignores invalid values', () => {
-      baseDefaults.text['font-family'].setValue('Helvetica');
-      baseDefaults.text['font-size'].setValue(12.15);
-      baseDefaults.text['font-weight'].setValue(380.2);
-      baseDefaults.text['font-style'].setValue('oblique');
+      defaults.text['font-family'].setValue('Helvetica');
+      defaults.text['font-size'].setValue(12.15);
+      defaults.text['font-weight'].setValue(380.2);
+      defaults.text['font-style'].setValue('oblique');
 
-      let saved = baseDefaults.toSaved();
+      let saved = defaults.toSaved();
       saved.text['font-family'] = 55;
       saved.text['font-size'] = -2;
       saved.text['font-weight'] = 'asdf';
       saved.text['font-style'] = 'zxcv';
 
-      baseDefaults.applySaved(saved);
-      expect(baseDefaults.text['font-family'].getValue()).toBe('Helvetica');
-      expect(baseDefaults.text['font-size'].getValue()).toBe(12.15);
-      expect(baseDefaults.text['font-weight'].getValue()).toBe(380.2);
-      expect(baseDefaults.text['font-style'].getValue()).toBe('oblique');
+      defaults.applySaved(saved);
+      expect(defaults.text['font-family'].getValue()).toBe('Helvetica');
+      expect(defaults.text['font-size'].getValue()).toBe(12.15);
+      expect(defaults.text['font-weight'].getValue()).toBe(380.2);
+      expect(defaults.text['font-style'].getValue()).toBe('oblique');
     });
 
     it('ignores undefined values', () => {
       // just test some values
-      baseDefaults.text['font-size'].setValue(8.8);
-      baseDefaults.text['font-weight'].setValue(566);
+      defaults.text['font-size'].setValue(8.8);
+      defaults.text['font-weight'].setValue(566);
 
-      let saved = baseDefaults.toSaved();
+      let saved = defaults.toSaved();
       saved.text['font-size'] = undefined;
       saved.text['font-weight'] = undefined;
 
-      baseDefaults.applySaved(saved);
-      expect(baseDefaults.text['font-size'].getValue()).toBe(8.8);
-      expect(baseDefaults.text['font-weight'].getValue()).toBe(566);
+      defaults.applySaved(saved);
+      expect(defaults.text['font-size'].getValue()).toBe(8.8);
+      expect(defaults.text['font-weight'].getValue()).toBe(566);
     });
   });
 });
