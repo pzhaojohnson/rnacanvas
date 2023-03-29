21bd033ff79ad8ca0bbdf326ac153ef84e4e074f
Philip Johnson <p.zhao.johnson@gmail.com>
2023-03-29 09:36:50 -0400

    Add empty PreviousVersionsForm

---
+++ src/forms/previous-versions/PreviousVersionsForm.test.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import { render } from 'react-dom';
+import { unmountComponentAtNode } from 'react-dom';
+
+import { act } from 'react-dom/test-utils';
+
+import { PreviousVersionsForm } from './PreviousVersionsForm';
+
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('PreviousVersionsForm component', () => {
+  act(() => {
+    render(<PreviousVersionsForm />, container);
+  });
+});
---
+++ src/forms/previous-versions/PreviousVersionsForm.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import styles from './PreviousVersionsForm.css';
+
+export function PreviousVersionsForm() {
+  return (
+    <div className={styles.previousVersionsForm} />
+  );
+}
