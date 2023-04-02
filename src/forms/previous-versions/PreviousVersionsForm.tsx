1bda185cf7044c9e09cf5a08c26110d78fa64a4f
Philip Johnson <p.zhao.johnson@gmail.com>
2023-04-02 13:54:14 -0400

    Add content to PreviousVersionsForm

--- src/forms/previous-versions/PreviousVersionsForm.tsx
+++ src/forms/previous-versions/PreviousVersionsForm.tsx
@@ -2,8 +2,18 @@ import * as React from 'react';
 
 import styles from './PreviousVersionsForm.css';
 
+import { Header } from './private/Header';
+
+import { LeadingNoteOnSavedDrawings } from './private/LeadingNoteOnSavedDrawings';
+
+import { PreviousVersionsList } from './private/PreviousVersionsList';
+
 export function PreviousVersionsForm() {
   return (
-    <div className={styles.previousVersionsForm} />
+    <div className={styles.previousVersionsForm} >
+      <Header />
+      <LeadingNoteOnSavedDrawings />
+      <PreviousVersionsList />
+    </div>
   );
 }
