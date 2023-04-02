53d0e815597b6204831342ca9bf51df84abbfc1a
Philip Johnson <p.zhao.johnson@gmail.com>
2023-04-02 16:02:14 -0400

    Edit text of note

--- src/forms/previous-versions/private/LeadingNoteOnSavedDrawings.tsx
+++ src/forms/previous-versions/private/LeadingNoteOnSavedDrawings.tsx
@@ -11,9 +11,7 @@ export function LeadingNoteOnSavedDrawings() {
 
   let mainText = (
     <p className={styles.mainText} >
-      Drawings from older versions of the app
-      can always be opened in later versions of the app,
-      but drawings from later versions of the app
+      Drawings from later versions of the app
       may not be compatible with older versions of the app.
     </p>
   );
