40a8653a634c78a2b6203e246ba11297da6c7553
Philip Johnson <p.zhao.johnson@gmail.com>
2023-04-09 11:57:50 -0400

    Add an arrow to version links

--- src/forms/previous-versions/private/VersionLink.tsx
+++ src/forms/previous-versions/private/VersionLink.tsx
@@ -6,6 +6,32 @@ import { VersionId } from './VersionId';
 
 import { Month } from './Month';
 
+function Arrow() {
+  let head = (
+    <path
+      className={styles.arrowHead}
+      d="M 8.75 0.75 L 13.25 0.75 L 13.25 5.75"
+      strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+      fill="none"
+    />
+  );
+
+  let shaft = (
+    <line
+      className={styles.arrowShaft}
+      x1="6.75" y1="7.25" x2="13.25" y2="0.75"
+      strokeWidth="1.5" strokeLinecap="round"
+    />
+  );
+
+  return (
+    <svg width="14px" height="12px" viewBox="0 0 14 12" >
+      {head}
+      {shaft}
+    </svg>
+  );
+}
+
 export type Props = {
   /**
    * A Google App Engine version ID.
@@ -41,6 +67,7 @@ export function VersionLink(props: Props) {
   return (
     <a {...{ className, href, target, rel, style }} >
       {month.fullName} {day}, {year}
+      <Arrow />
     </a>
   );
 }
