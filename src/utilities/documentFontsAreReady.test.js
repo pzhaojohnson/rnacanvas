99a20786138e17e7a558160633124376e139bd76
Philip Johnson <p.zhao.johnson@gmail.com>
2023-04-23 17:30:56 -0400

    Remove unused documentFontsAreReady function

--- src/utilities/documentFontsAreReady.test.js
+++
@@ -1,7 +0,0 @@
-import { documentFontsAreReady } from './documentFontsAreReady';
-
-test('documentFontsAreReady function', () => {
-  // just a smoke test
-  // (seems difficult to test fonts-related functionality on Node.js)
-  expect(documentFontsAreReady()).toBe(false);
-});
