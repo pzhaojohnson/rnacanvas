4957065792ab5944feb59b29873cdb6555fe9b4d
Philip Johnson <p.zhao.johnson@gmail.com>
2023-04-23 14:59:44 -0400

    Initialize app in a timeout

--- src/index.tsx
+++ src/index.tsx
@@ -13,44 +13,46 @@ import { userIsTyping } from 'Utilities/userIsTyping';
 let loadingScreen = new LoadingScreen();
 loadingScreen.show();
 
-let app = new App();
-
-// page will probably be done loading after 2.5 seconds
 setTimeout(() => {
-  loadingScreen.hide().then(() => {
-    app.appendTo(document.body);
-    app.formContainer.renderForm(() => <WelcomePage app={app} />);
+  let app = new App();
+
+  // page will probably be done loading after 2.5 seconds
+  setTimeout(() => {
+    loadingScreen.hide().then(() => {
+      app.appendTo(document.body);
+      app.formContainer.renderForm(() => <WelcomePage app={app} />);
+    });
+  }, 2500);
+
+  // disable drag and drop
+  document.body.ondragstart = () => false;
+  document.body.ondrop = () => false;
+
+  // prevent text selection after double-click
+  // when the user is not typing
+  document.addEventListener('mousedown', event => {
+    // cannot simply listen for the dblclick event since text selection
+    // seems to happen before dblclick events are dispatched
+    if (event.detail > 1 && !userIsTyping()) {
+      event.preventDefault();
+    }
+  }, false);
+
+  // ask for confirmation before leaving if the drawing is nonempty
+  // and the app setting is set to do so
+  window.addEventListener('beforeunload', event => {
+    if (app.drawing.isEmpty()) {
+      return;
+    }
+
+    let askBeforeLeaving = app.settings.askBeforeLeaving;
+    if (askBeforeLeaving != undefined && !askBeforeLeaving) {
+      // only if explicitly set to false
+      return;
+    }
+
+    let message = 'Are you sure?';
+    (event || window.event).returnValue = message;
+    return message;
   });
-}, 2500);
-
-// disable drag and drop
-document.body.ondragstart = () => false;
-document.body.ondrop = () => false;
-
-// prevent text selection after double-click
-// when the user is not typing
-document.addEventListener('mousedown', event => {
-  // cannot simply listen for the dblclick event since text selection
-  // seems to happen before dblclick events are dispatched
-  if (event.detail > 1 && !userIsTyping()) {
-    event.preventDefault();
-  }
-}, false);
-
-// ask for confirmation before leaving if the drawing is nonempty
-// and the app setting is set to do so
-window.addEventListener('beforeunload', event => {
-  if (app.drawing.isEmpty()) {
-    return;
-  }
-
-  let askBeforeLeaving = app.settings.askBeforeLeaving;
-  if (askBeforeLeaving != undefined && !askBeforeLeaving) {
-    // only if explicitly set to false
-    return;
-  }
-
-  let message = 'Are you sure?';
-  (event || window.event).returnValue = message;
-  return message;
-});
+}, 50);
