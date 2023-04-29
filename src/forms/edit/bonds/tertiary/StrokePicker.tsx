bfe97aa328fda3673f212a1bd19e6d75e745e0b7
Philip Johnson <p.zhao.johnson@gmail.com>
2023-04-29 10:38:43 -0400

    Stop updating default stroke for tertiary bonds in StrokePicker component

--- src/forms/edit/bonds/tertiary/StrokePicker.tsx
+++ src/forms/edit/bonds/tertiary/StrokePicker.tsx
@@ -1,21 +1,10 @@
 import type { App } from 'App';
 
-import { TertiaryBond } from 'Draw/bonds/curved/TertiaryBond';
-
-import * as SVG from '@svgdotjs/svg.js';
+import type { TertiaryBond } from 'Draw/bonds/curved/TertiaryBond';
 
 import * as React from 'react';
 
 import { ColorAttributePicker } from 'Forms/edit/svg/ColorAttributePicker';
-
-import { colorValuesAreEqual } from 'Draw/svg/colorValuesAreEqual';
-
-/**
- * Returns true if the color is white.
- */
-function isWhite(color: SVG.Color): boolean {
-  return colorValuesAreEqual(color.toHex(), '#ffffff');
-}
 
 export type Props = {
   /**
@@ -38,13 +27,6 @@ export function StrokePicker(props: Props) {
         props.app.pushUndo();
       }}
       onEdit={event => {
-        let newValue = event.newValue;
-
-        // don't make the same color as the background by default
-        if (!isWhite(newValue)) {
-          TertiaryBond.recommendedDefaults.path['stroke'] = newValue.toHex();
-        }
-
         props.app.refresh(); // refresh after updating all values
       }}
     />
