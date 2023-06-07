925406c096ac3f6ed46876c4ef7aae70cd20825d
Philip Johnson <p.zhao.johnson@gmail.com>
2023-06-07 16:24:54 -0400

    Add primaryBonds getter and appendPrimaryBond method

--- src/draw/fragments/DrawingFragment.test.js
+++ src/draw/fragments/DrawingFragment.test.js
@@ -1,6 +1,25 @@
+/**
+ * Makes it possible to set certain SVG element coordinates.
+ */
+import * as SVG from 'Draw/svg/NodeSVG';
+
 import { BasicDrawingFragment } from './private/BasicDrawingFragment';
 
+import { createBase } from 'Draw/bases/createBase';
+
+import { createPrimaryBond } from 'Draw/bonds/straight/createPrimaryBond';
+
 import { DrawingFragment } from './DrawingFragment';
+
+let drawingFragment = null;
+
+beforeEach(() => {
+  drawingFragment = new DrawingFragment();
+});
+
+afterEach(() => {
+  drawingFragment = null;
+});
 
 describe('DrawingFragment class', () => {
   describe('constructor', () => {
@@ -8,5 +27,22 @@ describe('DrawingFragment class', () => {
       let drawingFragment = new DrawingFragment();
       expect(drawingFragment.wrappee).toBeInstanceOf(BasicDrawingFragment);
     });
+  });
+
+  test('primaryBonds getter and appendPrimaryBond method', () => {
+    expect(drawingFragment.primaryBonds).toStrictEqual([]);
+
+    let bases = [createBase('a'), createBase('G'), createBase('c')];
+
+    let pb1 = createPrimaryBond({ base1: bases[0], base2: bases[1] });
+    let pb2 = createPrimaryBond({ base1: bases[1], base2: bases[0] });
+    let pb3 = createPrimaryBond({ base1: bases[0], base2: bases[2] });
+
+    drawingFragment.appendPrimaryBond(pb1);
+    expect(drawingFragment.primaryBonds).toStrictEqual([pb1]);
+
+    drawingFragment.appendPrimaryBond(pb2);
+    drawingFragment.appendPrimaryBond(pb3);
+    expect(drawingFragment.primaryBonds).toStrictEqual([pb1, pb2, pb3]);
   });
 });
--- src/draw/fragments/DrawingFragment.ts
+++ src/draw/fragments/DrawingFragment.ts
@@ -1,5 +1,7 @@
 import { BasicDrawingFragment } from './private/BasicDrawingFragment';
 
+import * as AppendPrimaryBond from './private/appendPrimaryBond';
+
 /**
  * Comparable to the document fragment class.
  */
@@ -8,4 +10,19 @@ export class DrawingFragment {
    * This class wraps a basic drawing fragment.
    */
   readonly wrappee = new BasicDrawingFragment();
+
+  get primaryBonds() {
+    return this.wrappee.primaryBonds;
+  }
+
+  appendPrimaryBond(
+    ...args: Parameters<
+      InstanceType<
+        typeof AppendPrimaryBond.DrawingFragmentDecorator
+      >['appendPrimaryBond']
+    >
+  ) {
+    return (new AppendPrimaryBond.DrawingFragmentDecorator(this.wrappee))
+      .appendPrimaryBond(...args);
+  }
 }
