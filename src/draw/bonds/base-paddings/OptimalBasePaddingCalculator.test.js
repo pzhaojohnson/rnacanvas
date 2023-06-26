885495bc1db135e550dc7aa5e6fbfa6799a1c740
Philip Johnson <p.zhao.johnson@gmail.com>
2023-06-26 11:44:31 -0400

    Add OptimalBasePaddingCalculator class

---
+++ src/draw/bonds/base-paddings/OptimalBasePaddingCalculator.test.js
@@ -0,0 +1,38 @@
+import { OptimalBasePaddingCalculator } from './OptimalBasePaddingCalculator';
+
+function createBaseMock() {
+  return {
+    text: {
+      bbox: () => ({
+        width: 0,
+        height: 0,
+      }),
+    },
+  };
+}
+
+let calculator = null;
+
+beforeEach(() => {
+  calculator = new OptimalBasePaddingCalculator();
+});
+
+afterEach(() => {
+  calculator = null;
+});
+
+describe('OptimalBasePaddingCalculator class', () => {
+  describe('calculatorFor method', () => {
+    test('when the text width of the base is bigger', () => {
+      let b = createBaseMock();
+      b.text.bbox = () => ({ width: 32, height: 22 });
+      expect(calculator.calculateFor(b)).toBeCloseTo(16);
+    });
+
+    test('when the text height of the base is bigger', () => {
+      let b = createBaseMock();
+      b.text.bbox = () => ({ width: 54, height: 62 });
+      expect(calculator.calculateFor(b)).toBeCloseTo(31);
+    });
+  });
+});
---
+++ src/draw/bonds/base-paddings/OptimalBasePaddingCalculator.ts
@@ -0,0 +1,20 @@
+export interface Base {
+  text: {
+    bbox(): {
+      width: number;
+      height: number;
+    };
+  };
+}
+
+export class OptimalBasePaddingCalculator {
+  /**
+   * Calculates what would be an optimal base padding for bonds
+   * attached to the base.
+   */
+  calculateFor(b: Base): number {
+    let bbox = b.text.bbox();
+    let size = Math.max(bbox.width, bbox.height);
+    return size / 2;
+  }
+}
