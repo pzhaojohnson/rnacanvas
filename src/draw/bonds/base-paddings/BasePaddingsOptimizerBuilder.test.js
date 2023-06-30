ca83a72cca06b8c51010420b8b196710e889999d
Philip Johnson <p.zhao.johnson@gmail.com>
2023-06-29 20:53:23 -0400

    Replace build method with build methods for specific bond types

--- src/draw/bonds/base-paddings/BasePaddingsOptimizerBuilder.test.js
+++ src/draw/bonds/base-paddings/BasePaddingsOptimizerBuilder.test.js
@@ -3,9 +3,21 @@ import { BasePaddingsOptimizer } from './BasePaddingsOptimizer';
 import { BasePaddingsOptimizerBuilder } from './BasePaddingsOptimizerBuilder';
 
 describe('BasePaddingsOptimizerBuilder class', () => {
-  test('build method', () => {
+  test('buildForPrimaryBonds method', () => {
     let builder = new BasePaddingsOptimizerBuilder();
-    let optimizer = builder.build();
+    let optimizer = builder.buildForPrimaryBonds();
+    expect(optimizer).toBeInstanceOf(BasePaddingsOptimizer);
+  });
+
+  test('buildForSecondaryBonds method', () => {
+    let builder = new BasePaddingsOptimizerBuilder();
+    let optimizer = builder.buildForSecondaryBonds();
+    expect(optimizer).toBeInstanceOf(BasePaddingsOptimizer);
+  });
+
+  test('buildForTertiaryBonds method', () => {
+    let builder = new BasePaddingsOptimizerBuilder();
+    let optimizer = builder.buildForTertiaryBonds();
     expect(optimizer).toBeInstanceOf(BasePaddingsOptimizer);
   });
 });
--- src/draw/bonds/base-paddings/BasePaddingsOptimizerBuilder.ts
+++ src/draw/bonds/base-paddings/BasePaddingsOptimizerBuilder.ts
@@ -3,16 +3,56 @@ import { BasePaddingsOptimizer } from './BasePaddingsOptimizer';
 import { BasePadding1Optimizer } from './BasePadding1Optimizer';
 import { BasePadding2Optimizer } from './BasePadding2Optimizer';
 
-import { OptimalBasePaddingCalculator } from './OptimalBasePaddingCalculator';
+import { OptimalBasePaddingCalculator2 } from './OptimalBasePaddingCalculator2';
 
 export class BasePaddingsOptimizerBuilder {
-  build() {
+  buildForPrimaryBonds() {
+    let scalingFactor = 0.689655172413793;
+
     return new BasePaddingsOptimizer({
       basePadding1Optimizer: new BasePadding1Optimizer({
-        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator(),
+        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator2({
+          scalingFactor,
+        }),
       }),
       basePadding2Optimizer: new BasePadding2Optimizer({
-        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator(),
+        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator2({
+          scalingFactor,
+        }),
+      }),
+    });
+  }
+
+  buildForSecondaryBonds() {
+    let scalingFactor = 0.541871921182266;
+
+    return new BasePaddingsOptimizer({
+      basePadding1Optimizer: new BasePadding1Optimizer({
+        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator2({
+          scalingFactor,
+        }),
+      }),
+      basePadding2Optimizer: new BasePadding2Optimizer({
+        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator2({
+          scalingFactor,
+        }),
+      }),
+    });
+  }
+
+  buildForTertiaryBonds() {
+    let scalingFactor = 0.7881773399014778;
+
+    return new BasePaddingsOptimizer({
+      basePadding1Optimizer: new BasePadding1Optimizer({
+        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator2({
+          scalingFactor,
+        }),
+      }),
+      basePadding2Optimizer: new BasePadding2Optimizer({
+        optimalBasePaddingCalculator: new OptimalBasePaddingCalculator2({
+          scalingFactor,
+        }),
       }),
     });
   }
