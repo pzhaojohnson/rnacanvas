be61195b6e48bb2d06b7b7f48ee7cda6ec5928e8
Philip Johnson <p.zhao.johnson@gmail.com>
2023-07-13 10:38:14 -0400

    Add StringInput component

---
+++ src/forms/edit/strings/StringInput.test.js
@@ -0,0 +1,231 @@
+import * as React from 'react';
+
+import { render } from 'react-dom';
+import { unmountComponentAtNode } from 'react-dom';
+
+import { act } from 'react-dom/test-utils';
+import { Simulate } from 'react-dom/test-utils';
+
+import { StringInput } from './StringInput';
+
+let props = null;
+
+let container = null;
+
+beforeEach(() => {
+  let initialValueProvider = {
+    provide: () => '',
+  };
+
+  let submittedValueRefiner = {
+    refine: () => '',
+  };
+
+  let shouldSetDecider = {
+    shouldSetTo: () => true,
+  };
+
+  let setter = {
+    setTo: () => {},
+  };
+
+  props = {
+    initialValueProvider, submittedValueRefiner, shouldSetDecider, setter,
+  };
+
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+
+  props = null;
+});
+
+describe('StringInput component', () => {
+  it('renders with the initial value provided by the provider', () => {
+    props.initialValueProvider.provide = () => '8123iuhwekbjw8913';
+
+    act(() => { render(<StringInput {...props} />, container); });
+
+    expect(container.firstChild.value).toBe('8123iuhwekbjw8913');
+  });
+
+  it('can set on blur', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.submittedValueRefiner.refine = () => '998ysdhk13kj398le';
+    props.shouldSetDecider.shouldSetTo = () => true;
+    props.setter.setTo = jest.fn();
+
+    Simulate.blur(container.firstChild);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(props.setter.setTo).toHaveBeenCalledTimes(1);
+        expect(props.setter.setTo.mock.calls[0][0]).toBe('998ysdhk13kj398le');
+        resolve();
+      }, 250);
+    });
+  });
+
+  it('can set on enter key up', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.submittedValueRefiner.refine = () => '989237YKJHEQKJ198';
+    props.shouldSetDecider.shouldSetTo = () => true;
+    props.setter.setTo = jest.fn();
+
+    Simulate.keyUp(container.firstChild, { key: 'Enter' });
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(props.setter.setTo).toHaveBeenCalledTimes(1);
+        expect(props.setter.setTo.mock.calls[0][0]).toBe('989237YKJHEQKJ198');
+        resolve();
+      }, 250);
+    });
+  });
+
+  it('passes the submitted value to the refiner', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    container.firstChild.value = '90D-3RNCXXC.XVUHA';
+    Simulate.change(container.firstChild, { bubbles: true });
+
+    props.submittedValueRefiner.refine = jest.fn(() => '');
+
+    Simulate.blur(container.firstChild);
+
+    expect(props.submittedValueRefiner.refine).toHaveBeenCalledTimes(1);
+    let call = props.submittedValueRefiner.refine.mock.calls[0];
+    expect(call[0]).toBe('90D-3RNCXXC.XVUHA');
+  });
+
+  it('refines the displayed value on submit', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.submittedValueRefiner.refine = () => '99831-fbiu3r98783cb';
+    props.shouldSetDecider.shouldSetTo = () => true;
+
+    Simulate.blur(container.firstChild);
+
+    expect(container.firstChild.value).toBe('99831-fbiu3r98783cb');
+  });
+
+  it('passes the refined submitted value to the should-set decider', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.submittedValueRefiner.refine = () => '9309sojnjc635613uyknsj';
+    props.shouldSetDecider.shouldSetTo = jest.fn(() => true);
+
+    Simulate.blur(container.firstChild);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(props.shouldSetDecider.shouldSetTo).toHaveBeenCalledTimes(1);
+        let call = props.shouldSetDecider.shouldSetTo.mock.calls[0];
+        expect(call[0]).toBe('9309sojnjc635613uyknsj');
+        resolve();
+      }, 250);
+    });
+  });
+
+  it('passes the refined submitted value to the setter', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.submittedValueRefiner.refine = () => '9v89hjngj3081313y';
+    props.shouldSetDecider.shouldSetTo = () => true;
+    props.setter.setTo = jest.fn();
+
+    Simulate.blur(container.firstChild);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(props.setter.setTo).toHaveBeenCalledTimes(1);
+        expect(props.setter.setTo.mock.calls[0][0]).toBe('9v89hjngj3081313y');
+        resolve();
+      }, 250);
+    });
+  });
+
+  it('does not set when the decider says not to', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.shouldSetDecider.shouldSetTo = () => false;
+    props.setter.setTo = jest.fn();
+
+    Simulate.blur(container.firstChild);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(props.setter.setTo).not.toHaveBeenCalled();
+
+        props.shouldSetDecider.shouldSetTo = () => true;
+
+        Simulate.blur(container.firstChild);
+
+        // now it does set
+        setTimeout(() => {
+          expect(props.setter.setTo).toHaveBeenCalledTimes(1);
+          resolve();
+        }, 250);
+      }, 250);
+    });
+  });
+
+  it('resets the displayed value on submission rejection', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    container.firstChild.value = 'asdf';
+    Simulate.change(container.firstChild, { bubbles: true });
+
+    props.initialValueProvider.provide = () => '889iu3kjfankdjknoci89';
+    props.shouldSetDecider.shouldSetTo = () => false;
+
+    Simulate.blur(container.firstChild);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(container.firstChild.value).toBe('889iu3kjfankdjknoci89');
+        resolve();
+      }, 250);
+    });
+  });
+
+  it('requeries the initial value provider on submission rejection', () => {
+    act(() => { render(<StringInput {...props} />, container); });
+
+    props.initialValueProvider.provide = jest.fn(() => 'asdf');
+    props.shouldSetDecider.shouldSetTo = () => false;
+
+    Simulate.blur(container.firstChild);
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        // should requery to get the most up-to-date value
+        expect(props.initialValueProvider.provide).toHaveBeenCalledTimes(1);
+        resolve();
+      }, 250);
+    });
+  });
+
+  it('renders with the provided ID', () => {
+    props.id = 'auiwuefhho2892398hiud';
+
+    act(() => { render(<StringInput {...props} />, container); });
+
+    expect(container.firstChild.id).toBe('auiwuefhho2892398hiud');
+  });
+
+  it('renders with the provided style props', () => {
+    props.style = { margin: '2px 0px 318px 532px' };
+
+    act(() => { render(<StringInput {...props} />, container); });
+
+    expect(container.firstChild.style.margin).toBe('2px 0px 318px 532px');
+  });
+});
---
+++ src/forms/edit/strings/StringInput.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+
+/**
+ * Already has CSS styles fitting in with the aesthetics of the app.
+ */
+import { TextInput as BasicTextInput } from 'Forms/inputs/text/TextInput';
+
+import { CSSProperties } from 'Forms/inputs/text/TextInput';
+
+export interface InitialValueProvider {
+  /**
+   * Returns an initial value for the string input component to be
+   * shown when first rendered.
+   *
+   * The returned value should convey to the user the current value
+   * of the subject string value for the string input component.
+   */
+  provide(): string;
+}
+
+export interface SubmittedValueRefiner {
+  /**
+   * Given a value submitted by the user, this method returns a refined
+   * version of it (such as with leading and trailing whitespace
+   * trimmed).
+   *
+   * This method is called right after the user submits a value and
+   * before the submitted value is processed further.
+   *
+   * Is meant to give an opportunity to refine a bit the value
+   * submitted by the user.
+   */
+  refine(submittedValue: string): string;
+}
+
+export interface ShouldSetDecider {
+  /**
+   * Is passed a value submitted by the user and refined by the
+   * refiner.
+   *
+   * Returns true if the subject string value of the string input
+   * component should be set to the given value and returns false
+   * otherwise.
+   */
+  shouldSetTo(refinedSubmittedValue: string): boolean;
+}
+
+export interface Setter {
+  /**
+   * Sets the subject string value of the string input component to
+   * the given value.
+   *
+   * Is passed the same value that was provided to the should-set
+   * decider immediately prior
+   *
+   * Is only called if the should-set decider says so.
+   */
+  setTo(refinedSubmittedValue: string): void;
+}
+
+export type Props = {
+  initialValueProvider: InitialValueProvider;
+
+  submittedValueRefiner: SubmittedValueRefiner;
+
+  shouldSetDecider: ShouldSetDecider;
+
+  setter: Setter;
+
+  id?: string;
+
+  style?: CSSProperties;
+};
+
+/**
+ * A text input element for controlling a string value in the app.
+ */
+export class StringInput extends React.Component<Props> {
+  state: {
+    value: string;
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      value: props.initialValueProvider.provide(),
+    };
+  }
+
+  render() {
+    return (
+      <BasicTextInput
+        value={this.state.value}
+        onChange={event => {
+          this.setState({ value: event.target.value });
+        }}
+        onBlur={() => {
+          this.submit();
+        }}
+        onKeyUp={event => {
+          if (event.key.toLowerCase() == 'enter') {
+            this.submit();
+          }
+        }}
+        id={this.props.id}
+        style={this.props.style}
+      />
+    );
+  }
+
+  submit() {
+    let submittedValue = this.state.value;
+
+    let refiner = this.props.submittedValueRefiner;
+    let refinedSubmittedValue = refiner.refine(submittedValue);
+
+    this.setState({ value: refinedSubmittedValue });
+
+    // give enough time for the above state change
+    setTimeout(() => {
+      if (this.props.shouldSetDecider.shouldSetTo(refinedSubmittedValue)) {
+        this.props.setter.setTo(refinedSubmittedValue);
+      } else {
+        this.resetValue();
+      }
+    }, 50);
+  }
+
+  resetValue() {
+    // requery to get the most up-to-date value
+    let value = this.props.initialValueProvider.provide();
+
+    this.setState({ value });
+  }
+}
