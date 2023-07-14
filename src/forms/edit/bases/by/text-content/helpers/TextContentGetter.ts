a4bc794754913e98c2fcb05bd2a6f688b7c87a2f
Philip Johnson <p.zhao.johnson@gmail.com>
2023-07-14 15:46:53 -0400

    Move Base interface back to TextContentGetter module

--- src/forms/edit/bases/by/text-content/helpers/TextContentGetter.ts
+++ src/forms/edit/bases/by/text-content/helpers/TextContentGetter.ts
@@ -1,4 +1,19 @@
-import { Base } from './Base';
+/**
+ * A base in a structure drawing.
+ */
+export interface Base {
+  /**
+   * The text element of the base.
+   *
+   * Assumed to contain the entire text content of the base.
+   */
+  text: {
+    /**
+     * Returns the text content of the text element of the base.
+     */
+    text(): string;
+  }
+}
 
 export class TextContentGetter {
   /**
