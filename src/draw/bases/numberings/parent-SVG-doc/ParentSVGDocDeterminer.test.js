c0a8d1cee85617fe4acd25090d44564a3e5f6033
Philip Johnson <p.zhao.johnson@gmail.com>
2023-07-28 16:25:23 -0400

    Use actual SVG document objects in units tests

--- src/draw/bases/numberings/parent-SVG-doc/ParentSVGDocDeterminer.test.js
+++ src/draw/bases/numberings/parent-SVG-doc/ParentSVGDocDeterminer.test.js
@@ -1,3 +1,5 @@
+import * as SVG from '@svgdotjs/svg.js';
+
 import { ParentSVGDocDeterminer } from './ParentSVGDocDeterminer';
 
 function createBaseNumberingMock() {
@@ -33,7 +35,7 @@ describe('ParentSVGDocDeterminer class', () => {
     });
 
     test('only the text element is in an SVG doc', () => {
-      baseNumbering.text.root = () => 'SVG doc - 2398r2983hiwedhj';
+      baseNumbering.text.root = () => new SVG.Svg();
       baseNumbering.line.root = () => null;
 
       expect(determiner.determineFor(baseNumbering)).toBeUndefined();
@@ -41,23 +43,26 @@ describe('ParentSVGDocDeterminer class', () => {
 
     test('only the line element is in an SVG doc', () => {
       baseNumbering.text.root = () => null;
-      baseNumbering.line.root = () => 'SVG doc - 98iudshfjkd';
+      baseNumbering.line.root = () => new SVG.Svg();
 
       expect(determiner.determineFor(baseNumbering)).toBeUndefined();
     });
 
     test('text and line elements are in the same SVG doc', () => {
-      baseNumbering.text.root = () => 'SVG doc - 238ur23hiwfejsd';
-      baseNumbering.line.root = () => 'SVG doc - 238ur23hiwfejsd';
+      let svgDoc = new SVG.Svg();
 
-      expect(determiner.determineFor(baseNumbering)).toBe(
-        'SVG doc - 238ur23hiwfejsd'
-      );
+      baseNumbering.text.root = () => svgDoc;
+      baseNumbering.line.root = () => svgDoc;
+
+      expect(determiner.determineFor(baseNumbering)).toBe(svgDoc);
+
+      // double-check
+      expect(svgDoc).toBeTruthy();
     });
 
     test('text and line elements are in different SVG docs', () => {
-      baseNumbering.text.root = () => 'SVG doc - 1';
-      baseNumbering.line.root = () => 'SVG doc - 2';
+      baseNumbering.text.root = () => new SVG.Svg();
+      baseNumbering.line.root = () => new SVG.Svg();
 
       expect(determiner.determineFor(baseNumbering)).toBeUndefined();
     });
