a01161430cdf620f2b914d2f3e06d8c4f9a7dae8
Philip Johnson <p.zhao.johnson@gmail.com>
2023-08-14 22:44:57 -0400

    Edit text of message

--- src/forms/edit/sequences/subsequences/add/subcomponents/RNA2DSchemaOriginMessage.tsx
+++ src/forms/edit/sequences/subsequences/add/subcomponents/RNA2DSchemaOriginMessage.tsx
@@ -13,15 +13,15 @@ export function RNA2DSchemaOriginMessage() {
   return (
     <div>
       <p className={styles.text} >
-        Adding subsequences not currently supported...
-      </p>
-      <div style={{ height: '33px' }} />
-      <p className={styles.text} >
-        Drawing originates from an RNA 2D schema...
+        Drawing was made from an RNA 2D schema...
       </p>
       <div style={{ height: '10px' }} />
       <p className={styles.text} style={{ marginLeft: '14px' }} >
         (e.g., comes from R2DT...)
+      </p>
+      <div style={{ height: '33px' }} />
+      <p className={styles.text} >
+        Adding subsequences is not currently supported...
       </p>
     </div>
   );
