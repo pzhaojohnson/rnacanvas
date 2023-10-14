6197a26e7c9b1a4d42583766922d6e1d4607104c
Philip Johnson <p.zhao.johnson@gmail.com>
2023-10-14 15:54:45 -0400

    Make constructor parameter unnamed

--- src/foreign/RNA2D/export/derive/PredeterminedValueDeriver.ts
+++ src/foreign/RNA2D/export/derive/PredeterminedValueDeriver.ts
@@ -1,16 +1,12 @@
-export type Config<T2> = {
-  predeterminedValue: T2;
-};
-
 /**
  * A way to always derive a certain value while still fulfilling a
  * value deriver interface.
  */
 export class PredeterminedValueDeriver<T1, T2> {
-  _config: Config<T2>;
+  _predeterminedValue: T2;
 
-  constructor(config: Config<T2>) {
-    this._config = config;
+  constructor(predeterminedValue: T2) {
+    this._predeterminedValue = predeterminedValue;
   }
 
   /**
@@ -18,6 +14,6 @@ export class PredeterminedValueDeriver<T1, T2> {
    * value-to-derive-from is.
    */
   deriveFrom(valueToDeriveFrom: T1): T2 {
-    return this._config.predeterminedValue;
+    return this._predeterminedValue;
   }
 }
