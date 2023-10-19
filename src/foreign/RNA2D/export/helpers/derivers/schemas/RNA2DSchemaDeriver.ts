71c86a843423acf7fd965df75cc0d4513541d8b7
Philip Johnson <p.zhao.johnson@gmail.com>
2023-10-19 08:45:18 -0400

    Rework RNA2DSchemaDeriver class

--- src/foreign/RNA2D/export/helpers/derivers/schemas/RNA2DSchemaDeriver.ts
+++ src/foreign/RNA2D/export/helpers/derivers/schemas/RNA2DSchemaDeriver.ts
@@ -1,32 +1,76 @@
-export type Helpers<RNA2DRNAMolecule> = {
+import type { App } from 'App';
+
+import type { Base as RNAcanvasBase } from 'Draw/bases/Base';
+
+export type RNAcanvasDrawing = InstanceType<typeof App>['drawing'];
+
+export interface TargetRNAcanvasDrawingGetter {
   /**
-   * Derives all corresponding RNA 2D RNA molecules from the relevant
-   * RNAcanvas drawing.
+   * Returns the target RNAcanvas drawing to derive an RNA 2D schema
+   * from.
    */
-  allRNA2DRNAMoleculesDeriver: {
-    derive(): RNA2DRNAMolecule[];
-  }
+  get(): RNAcanvasDrawing;
+}
+
+export interface AllRNAcanvasBasesInOrderFinder {
+  /**
+   * Returns all bases in the RNAcanvas drawing as they are ordered
+   * in the RNAcanvas drawing (e.g., taking into account the
+   * sequences in the drawing and the ordering of those sequences).
+   */
+  findIn(rnaCanvasDrawing: RNAcanvasDrawing): RNAcanvasBase[];
+}
+
+export type Helpers = {
+  targetRNAcanvasDrawingGetter: TargetRNAcanvasDrawingGetter;
+
+  allRNAcanvasBasesInOrderFinder: AllRNAcanvasBasesInOrderFinder;
 };
 
-export class RNA2DSchemaDeriver<RNA2DRNAMolecule> {
-  _helpers: Helpers<RNA2DRNAMolecule>;
+export class RNA2DSchemaDeriver {
+  _helpers: Helpers;
 
-  constructor(helpers: Helpers<RNA2DRNAMolecule>) {
+  constructor(helpers: Helpers) {
     this._helpers = helpers;
   }
 
-  /**
-   * Derives an RNA 2D schema from the relevant RNAcanvas drawing.
-   *
-   * The derived RNA 2D schema should be JSON serializable.
-   */
   derive() {
+    let targetRNAcanvasDrawing = this._helpers.targetRNAcanvasDrawingGetter.get();
+
+    let allRNAcanvasBasesInOrder = this._helpers.allRNAcanvasBasesInOrderFinder.findIn(targetRNAcanvasDrawing);
+
+    // don't export 5' and 3' labels as RNA 2D residues
+    // (might cause issues outside of RNAcanvas with R2DT)
+    allRNAcanvasBasesInOrder = allRNAcanvasBasesInOrder.filter(b => b.text.text() != "5'");
+    allRNAcanvasBasesInOrder = allRNAcanvasBasesInOrder.filter(b => b.text.text() != "3'");
+
+    let allRNA2DResidues = allRNAcanvasBasesInOrder.map((b, i) => ({
+      residueIndex: i,
+      residueName: b.text.text(),
+      templateResidueName: b.text.text(),
+      x: b.text.bbox().cx,
+      y: b.text.bbox().cy,
+    }));
+
+    // currently RNA 2D base-pairs must be labeled "canonical"
+    // (to not be ignored by R2DT)
+    const basePairType = 'canonical';
+
+    let allRNA2DBasePairs = targetRNAcanvasDrawing.secondaryBonds.map(sb => ({
+      basePairType,
+      residueIndex1: allRNAcanvasBasesInOrder.indexOf(sb.base1),
+      residueIndex2: allRNAcanvasBasesInOrder.indexOf(sb.base2),
+    }));
+
     return {
-      classes: [],
       rnaComplexes: [
         {
-          name: 'complex',
-          rnaMolecules: this._helpers.allRNA2DRNAMoleculesDeriver.derive(),
+          rnaMolecules: [
+            {
+              sequence: allRNA2DResidues,
+              basePairs: allRNA2DBasePairs,
+            },
+          ],
         },
       ],
     };
