d35ac83c5935d70ecdeb1b6af35382d2d14060d2
Philip Johnson <p.zhao.johnson@gmail.com>
2024-01-01 15:37:48 -0500

    Adjust appearance of latest updates notice

--- src/forms/welcome/helpers/LatestUpdatesNotice.tsx
+++ src/forms/welcome/helpers/LatestUpdatesNotice.tsx
@@ -2,6 +2,14 @@ import * as React from 'react';
 
 import styles from './LatestUpdatesNotice.css';
 
+function LatestUpdatesDate() {
+  return (
+    <p className={styles.latestUpdatesDate} >
+      (Dec. 2, 2023)
+    </p>
+  );
+}
+
 function LatestUpdatesLink() {
   return (
     <a
@@ -15,18 +23,51 @@ function LatestUpdatesLink() {
   );
 }
 
-function LatestUpdatesDate() {
+function LinkArrow() {
+  let head = (
+    <path
+      className={styles.linkArrowHead}
+      d="M 8.75 0.75 L 13.25 0.75 L 13.25 5.75"
+      strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+      fill="none"
+    />
+  );
+
+  let shaft = (
+    <line
+      className={styles.linkArrowShaft}
+      x1="6.75" y1="7.25" x2="13.25" y2="0.75"
+      strokeWidth="1.5" strokeLinecap="round"
+    />
+  );
+
   return (
-    <em className={styles.latestUpdatesDate} >
-      (Dec. 2, 2023)
-    </em>
+    <svg className={styles.linkArrow} viewBox="0 0 14 12" >
+      {head}
+      {shaft}
+    </svg>
+  );
+}
+
+function LatestUpdatesLinkWithArrow() {
+  return (
+    <div className={styles.latestUpdatesLinkWithArrow} >
+      <LatestUpdatesLink />
+      <LinkArrow />
+    </div>
   );
 }
 
 export function LatestUpdatesNotice() {
-  return (
-    <p className={styles.latestUpdatesNotice} >
-      See the <LatestUpdatesLink />! <LatestUpdatesDate />
+  let interveningText = (
+    <p className={styles.interveningText} >
+      See the
     </p>
+  );
+
+  return (
+    <div className={styles.latestUpdatesNotice} >
+      <LatestUpdatesDate />&nbsp;{interveningText}&nbsp;<LatestUpdatesLinkWithArrow />
+    </div>
   );
 }
