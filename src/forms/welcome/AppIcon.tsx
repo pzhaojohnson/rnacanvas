b78e71096d6c493cec47f618112a87e764f9702f
Philip Johnson <p.zhao.johnson@gmail.com>
2024-01-18 16:39:59 -0500

    Reexpress app icon in JSX and adjust colors slightly

--- src/forms/welcome/AppIcon.tsx
+++ src/forms/welcome/AppIcon.tsx
@@ -1,47 +1,54 @@
 import * as React from 'react';
+
 import styles from './AppIcon.css';
 
-const svg = `
-  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    viewBox="0 0 192 192" style="enable-background:new 0 0 192 192;" xml:space="preserve">
-  <style type="text/css">
-    .st0{fill:none;stroke:#8781BD;stroke-width:12;stroke-miterlimit:8;}
-    .st1{fill:#161138;}
-    .st2{fill:#8781BD;}
-  </style>
-  <line class="st0" x1="16" y1="96" x2="96" y2="96"/>
-  <line class="st0" x1="96" y1="96" x2="176" y2="96"/>
-  <line class="st0" x1="96" y1="96" x2="96" y2="176"/>
-  <line class="st0" x1="96" y1="16" x2="96" y2="96"/>
-  <line class="st0" x1="39.4" y1="39.4" x2="96" y2="96"/>
-  <line class="st0" x1="152.6" y1="39.4" x2="96" y2="96"/>
-  <circle class="st1" cx="16" cy="96" r="16"/>
-  <circle class="st1" cx="96" cy="16" r="16"/>
-  <circle class="st1" cx="39.4" cy="39.4" r="16"/>
-  <circle class="st1" cx="152.6" cy="39.4" r="16"/>
-  <circle class="st1" cx="176" cy="96" r="16"/>
-  <circle class="st1" cx="96" cy="176" r="16"/>
-  <circle class="st2" cx="96" cy="96" r="16"/>
-  </svg>
-`;
+type LoopProps = {
+  cx: string;
+  cy: string;
+  r: string;
+};
 
-// necessary for unit testing with Jest
-if (URL.createObjectURL == undefined) {
-  console.error('URL.createObjectURL static method is undefined.');
-  Object.defineProperty(URL, 'createObjectURL', { value: () => {} });
-  console.error('Placeholder function assigned to URL.createObjectURL.');
+function PeripheralLoop(props: LoopProps) {
+  return <circle className={styles.peripheralLoop} {...props} />;
 }
 
-const url = URL.createObjectURL(
-  new Blob([svg], { type: 'image/svg+xml' })
-);
+function CentralLoop(props: LoopProps) {
+  return <circle className={styles.centralLoop} {...props} />;
+}
+
+type StemProps = {
+  x1: string;
+  y1: string;
+  x2: string;
+  y2: string;
+};
+
+function Stem(props: StemProps) {
+  return <line className={styles.stem} {...props} />;
+}
 
 export function AppIcon() {
+  let xmlns = 'http://www.w3.org/2000/svg';
+
+  let className = styles.appIcon;
+
+  let viewBox = '0 0 192 192';
+
   return (
-    <img
-      className={styles.appIcon}
-      src={url}
-      alt='Icon'
-    />
+    <svg {...{ xmlns, className, viewBox }} >
+      <Stem x1='16' y1='96' x2='96' y2='96' />
+      <Stem x1='96' y1='96' x2='176' y2='96' />
+      <Stem x1='96' y1='96' x2='96' y2='176' />
+      <Stem x1='96' y1='16' x2='96' y2='96' />
+      <Stem x1='39.4' y1='39.4' x2='96' y2='96' />
+      <Stem x1='152.6' y1='39.4' x2='96' y2='96' />
+      <PeripheralLoop cx='16' cy='96' r='16' />
+      <PeripheralLoop cx='96' cy='16' r='16' />
+      <PeripheralLoop cx='39.4' cy='39.4' r='16' />
+      <PeripheralLoop cx='152.6' cy='39.4' r='16' />
+      <PeripheralLoop cx='176' cy='96' r='16' />
+      <PeripheralLoop cx='96' cy='176' r='16' />
+      <CentralLoop cx='96' cy='96' r='16' />
+    </svg>
   );
 }
